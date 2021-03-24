3ff35ecac5cc7459ebcc73ace3638d35c2b823d4
naodevtech <naimtahouri@gmail.com>
2021-03-24 02:24:19 +0100

    :boom: init swagger UI & morgan logger  :boom:

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+
+import YAML from 'yamljs';
+
+import swaggerUi from 'swagger-ui-express';
+const swaggerOptions = YAML.load('./swagger.yml');
 
 export const createServer = async () => {
   const server: express.Application = express();
 
   server.use(express.json());
 
+  server.use(morgan('tiny'));
+
   router(server);
+
+  server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
   return server;
 };
